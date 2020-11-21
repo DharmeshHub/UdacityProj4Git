77cfb61d49232a326345ae7825cda1381aebf340
DharmeshHub <73326514+DharmeshHub@users.noreply.github.com>
2020-11-21 12:54:54 -0600

    Update main.py

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -6,23 +6,50 @@ import socket
 import sys
 import logging
 from datetime import datetime
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure import metrics_exporter
+from opencensus.stats import aggregation as aggregation_module
+from opencensus.stats import measure as measure_module
+from opencensus.stats import stats as stats_module
+from opencensus.stats import view as view_module
+from opencensus.tags import tag_map as tag_map_module
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.trace.tracer import Tracer
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 
 # App Insights
 # TODO: Import required libraries for App Insights
 
 # Logging
-logger = # TODO: Setup logger
+# keep stdout/stderr logging using StreamHandler
+logger = logging.getLogger(__name__)
+
+# TODO: replace the all-zero GUID with your instrumentation key.
+logger.addHandler(AzureLogHandler(
+    connection_string='InstrumentationKey=25578d7a-2f3e-4f63-aa97-39b91737dee7')
+)
 
 # Metrics
-exporter = # TODO: Setup exporter
+exporter = metrics_exporter.new_metrics_exporter(
+  enable_standard_metrics=True,
+  connection_string='InstrumentationKey=25578d7a-2f3e-4f63-aa97-39b91737dee7')
 
 # Tracing
-tracer = # TODO: Setup tracer
+tracer = Tracer(
+    exporter=AzureExporter(
+        connection_string='InstrumentationKey=25578d7a-2f3e-4f63-aa97-39b91737dee7'),
+    sampler=ProbabilitySampler(1.0)
+)
 
 app = Flask(__name__)
-
+ 
 # Requests
-middleware = # TODO: Setup flask middleware
+middleware = FlaskMiddleware(
+    app,
+    exporter=AzureExporter(connection_string="InstrumentationKey=25578d7a-2f3e-4f63-aa97-39b91737dee7"),
+    sampler=ProbabilitySampler(rate=1.0)
+)
 
 # Load configurations from environment or config file
 app.config.from_pyfile('config_file.cfg')
@@ -61,27 +88,34 @@ def index():
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
         # TODO: use tracer object to trace cat vote
+        
+        # tracer.span(name=str(vote1))
+        with tracer.span(name='dogs') as span:
+            print('Dogs!')
         vote2 = r.get(button2).decode('utf-8')
         # TODO: use tracer object to trace dog vote
-
+        # tracer.span(name=str(vote2))
+        with tracer.span(name='cats') as span:
+            print('Cats!')
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
     elif request.method == 'POST':
 
         if request.form['vote'] == 'reset':
-
             # Empty table and return results
             r.set(button1,0)
             r.set(button2,0)
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
             # TODO: use logger object to log cat vote
-
+            logger.setLevel(logging.INFO)
+            logger.log(properties)
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
             # TODO: use logger object to log dog vote
-
+            logger.setLevel(logging.INFO)
+            logger.log(properties)
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
         else:
@@ -89,16 +123,22 @@ def index():
             # Insert vote result into DB
             vote = request.form['vote']
             r.incr(vote,1)
-
             # Get current values
             vote1 = r.get(button1).decode('utf-8')
+            # tracer.span(name=str(vote1))
+            with tracer.span(name='dogs') as span:
+                print('Dogs1!')
+            
             vote2 = r.get(button2).decode('utf-8')
+            # tracer.span(name=str(vote2))
+            with tracer.span(name='cats') as span:
+                print('Cats1!')
 
             # Return results
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
 if __name__ == "__main__":
     # comment line below when deploying to VMSS
-    app.run() # local
+    # app.run() # local
     # uncomment the line below before deployment to VMSS
-    # app.run(host='0.0.0.0', threaded=True, debug=True) # remote
+     app.run(host='0.0.0.0', threaded=True, debug=True) # remote
