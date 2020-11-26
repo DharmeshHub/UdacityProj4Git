1359aeb8758fbf4de4c64e2e3d94b8aa2094f3f4
DharmeshHub <73326514+DharmeshHub@users.noreply.github.com>
2020-11-26 10:34:21 -0600

    Update main.py

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -27,18 +27,18 @@ logger = logging.getLogger(__name__)
 
 # TODO: replace the all-zero GUID with your instrumentation key.
 logger.addHandler(AzureLogHandler(
-    connection_string='InstrumentationKey=7c5b861a-1af8-4bbd-9488-c03ca464102c;IngestionEndpoint=https://centralus-0.in.applicationinsights.azure.com/')
+    connection_string='InstrumentationKey=7c5b861a-1af8-4bbd-9488-c03ca464102c')
 )
 
 # Metrics
 exporter = metrics_exporter.new_metrics_exporter(
   enable_standard_metrics=True,
-  connection_string='InstrumentationKey=7c5b861a-1af8-4bbd-9488-c03ca464102c;IngestionEndpoint=https://centralus-0.in.applicationinsights.azure.com/')
+  connection_string='InstrumentationKey=7c5b861a-1af8-4bbd-9488-c03ca464102c')
 
 # Tracing
 tracer = Tracer(
     exporter=AzureExporter(
-        connection_string='InstrumentationKey=7c5b861a-1af8-4bbd-9488-c03ca464102c;IngestionEndpoint=https://centralus-0.in.applicationinsights.azure.com/'),
+        connection_string='InstrumentationKey=7c5b861a-1af8-4bbd-9488-c03ca464102c'),
     sampler=ProbabilitySampler(1.0)
 )
 
@@ -47,7 +47,7 @@ app = Flask(__name__)
 # Requests
 middleware = FlaskMiddleware(
     app,
-    exporter=AzureExporter(connection_string="InstrumentationKey=7c5b861a-1af8-4bbd-9488-c03ca464102c;IngestionEndpoint=https://centralus-0.in.applicationinsights.azure.com/"),
+    exporter=AzureExporter(connection_string="InstrumentationKey=7c5b861a-1af8-4bbd-9488-c03ca464102c"),
     sampler=ProbabilitySampler(rate=1.0)
 )
 
